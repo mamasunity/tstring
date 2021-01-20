31884d4789b4d6cdaf3e94255cddf81789c20c3b
mamasunity <mamasunity@users.noreply.github.com>
2021-01-21 08:31:25 +0900

    Add test on padding

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
+import (
+	"strings"
+)
+
 func Center(str string, length int, pad string) string {
 	if len(str) >= length {
 		return str
 	} else {
-		return ""
+		restPadding := length - len(str)
+		leftPadding := restPadding / 2
+
+		rightPadding := restPadding / 2
+		if restPadding%2 == 1 {
+			rightPadding++
+		}
+
+		return strings.Repeat(" ", leftPadding) + str + strings.Repeat(" ", rightPadding)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,23 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestCenter(t *testing.T) {
+func TestCenter1(t *testing.T) {
 	expect := "hello"
 	actual := Center("hello", 4, " ")
 
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf("\"%s \"!=\" %s\"", expect, actual)
+	}
+}
+
+func TestCenter2(t *testing.T) {
+	expect := "  hello   "
+	actual := Center("hello", 10, " ")
+
+	if expect != actual {
+		t.Errorf("\"%s \"!=\" %s\"", expect, actual)
 	}
 }
