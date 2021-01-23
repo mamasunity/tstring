b52974310078fc15ca9f760e17fdd86e2d77d55f
mamasunity <mamasunity@users.noreply.github.com>
2021-01-23 20:27:55 +0900

    Think tabSize is 0

--- main.go
+++ main.go
@@ -23,6 +23,10 @@ func Center(str string, length int, pad string) string {
 
 // ExpandTabs replace from tab to some space.
 func ExpandTabs(str string, tabSize int) string {
+	if tabSize <= 0 {
+		panic("tabSize should greater than 0 or equal 0")
+	}
+
 	return strings.ReplaceAll(str, "\t", strings.Repeat(" ", tabSize))
 }
 
