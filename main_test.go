a7d0bb86a93f5fee6204f4bd1f452cb9be582694
mamasunity <mamasunity@users.noreply.github.com>
2021-01-24 23:29:48 +0900

    Summarize test

--- main_test.go
+++ main_test.go
@@ -6,54 +6,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCenter1(t *testing.T) {
+func TestCenter(t *testing.T) {
 	assert.Equal(t, "hello", Center("hello", 4, " "))
-}
-
-func TestCenter2(t *testing.T) {
 	assert.Equal(t, "  hello   ", Center("hello", 10, " "))
-}
-
-func TestCenter3(t *testing.T) {
 	assert.Equal(t, "12hello123", Center("hello", 10, "123"))
 }
 
-func TestExpandTabs1(t *testing.T) {
+func TestExpandTabs(t *testing.T) {
 	assert.Equal(t, "a    bc    def", ExpandTabs("a\tbc\tdef", 4))
-}
-
-func TestExpandTabs2(t *testing.T) {
 	assert.Equal(t, "a  bc  def", ExpandTabs("a\tbc\tdef", 2))
-}
-
-func TestExpandTabs3(t *testing.T) {
 	assert.Equal(t, "中    文", ExpandTabs("中\t文", 4))
 }
 
-func TestFirstRuneLower1(t *testing.T) {
+func TestFirstRuneLower(t *testing.T) {
 	assert.Equal(t, "aBC", FirstRuneToLower("ABC"))
-}
-
-func TestFirstRuneLower2(t *testing.T) {
 	assert.Equal(t, "abc", FirstRuneToLower("abc"))
 }
 
-func TestFirstRuneToUpper1(t *testing.T) {
+func TestFirstRuneToUpper(t *testing.T) {
 	assert.Equal(t, "ABC", FirstRuneToUpper("aBC"))
-}
-
-func TestFirstRuneToUpper2(t *testing.T) {
 	assert.Equal(t, "ABC", FirstRuneToUpper("ABC"))
 }
 
-func TestInsert1(t *testing.T) {
+func TestInsert(t *testing.T) {
 	assert.Equal(t, "Aabc", Insert("abc", "A", 0))
-}
-
-func TestInsert2(t *testing.T) {
 	assert.Equal(t, "aAbc", Insert("abc", "A", 1))
-}
 
-func TestInsert3(t *testing.T) {
 	assert.Panics(t, func() { Insert("abc", "A", 10) }, "Index must be included range.")
 }
